9ac33ad3e8c3d51bd3738f7a711c5357be17dd45
PenguinL <diana1997525@gmail.com>
2021-09-14 17:38:27 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,1 +1,17 @@
+#in venv
+. venv/bin/activate
+#pip install -r req.txt
+pip freeze > req.txt
+chmod u+x *.sh *.py
 #git clone -o vue_sampl https://github.com/Kovalsky95/vue_sampl.git
+#git config --global user.email "diana1997525@gmail.com"
+#git config --global user.name "Kovalsky95"
+#git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=25200"
+#git config credential.helper store
+#git config --unset credential.helper
+git pull --all
+git add .
+git commit -m PenguinL
+git push --set-upstream vue_sampl master
+#deactivate
