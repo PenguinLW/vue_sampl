eec7e7963db7eeb333fe113a4edfab2737a8c0dc
Kovalsky95 <diana1997525@gmail.com>
2021-10-06 18:40:14 +0800

    PenguinL

--- set_env.sh
+++ set_env.sh
@@ -2,5 +2,6 @@
 . venv/bin/activate &&. nenv/bin/activate
 npm install vue@next
 npm install -D @vue/compiler-sfc
-#npm install -g @vue/cli
-#vue upgrade --next
+##npm install -g @vue/cli
+##vue upgrade --next
+#npm fund
