e088f9278997a95597a75d588a58f25678849814
Kovalsky95 <diana1997525@gmail.com>
2021-10-06 18:55:32 +0800

    PenguinL

--- app_run.sh
+++ app_run.sh
@@ -1,2 +1,5 @@
 #in venv
 . venv/bin/activate &&. nenv/bin/activate
+cd vue_sampl
+npm install
+npm run dev
--- set_env.sh
+++ set_env.sh
@@ -5,3 +5,9 @@ npm install -D @vue/compiler-sfc
 ##npm install -g @vue/cli
 ##vue upgrade --next
 #npm fund
+
+npm init vite@latest vue_sampl -- --template vue #npm init vite@latest vue_sampl --template vue
+
+cd vue_sampl
+npm install
+npm run dev
