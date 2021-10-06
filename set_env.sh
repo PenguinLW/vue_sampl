01c55b95ef17cd56607c4b887a0d53fc79c84293
Kovalsky95 <diana1997525@gmail.com>
2021-10-06 19:39:55 +0800

    PenguinL

--- set_env.sh
+++ set_env.sh
@@ -2,12 +2,14 @@
 . venv/bin/activate &&. nenv/bin/activate
 npm install vue@next
 npm install -D @vue/compiler-sfc
-##npm install -g @vue/cli
+npm install -g @vue/cli
 ##vue upgrade --next
-#npm fund
+npm fund
 
 npm init vite@latest vue_sampl -- --template vue #npm init vite@latest vue_sampl --template vue
 
 cd vue_sampl
 npm install
 npm run dev
+#deactivate
+#deactivate_node
