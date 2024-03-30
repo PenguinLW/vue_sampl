095bc35eb06da306acc0bfd34a79a61ebce7b9d8
PenguinLW <diana1997525@gmail.com>
2024-03-30 22:54:14 +0800

    PenguinL

--- app_run.sh
+++
@@ -1,5 +0,0 @@
-#in venv
-. venv/bin/activate &&. nenv/bin/activate
-cd sampl
-npm install
-npm run dev
--- set_env.sh
+++ set_env.sh
@@ -1,16 +1,12 @@
 #in venv
-. venv/bin/activate &&. nenv/bin/activate
+source ~/venv/bin/activate && source ~/nenv/bin/activate #. venv/bin/activate &&. nenv/bin/activate
 npm install vue@next
 npm install -D @vue/compiler-sfc
 npm install -g @vue/cli
 ##vue upgrade --next
 npm fund
 
-npm init vite@latest sampl -- --template vue #npm init vite@latest sampl --template vue
-
-##cd sampl
-##npm install
-##npm run dev -- --host
+#npm init vite@latest {project_name} -- --template vue #npm init vite@latest {project_name} --template vue
 
 #deactivate
 #deactivate_node
